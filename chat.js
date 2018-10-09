0e40229c350a1a266c3b11ced2a547b3bacf31fc
Moorad <34781684+mooradal@users.noreply.github.com>
2018-10-09 23:00:42 +0100

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,17 @@
+socket = io.connect("https://messaging-hkmipotzbi.now.sh/");
+
+var message = document.getElementById("message")
+var btn = document.getElementById("send")
+var senderName = document.getElementById("handle")
+var messages = document.getElementById("output")
+
+btn.addEventListener("click", function() {
+    socket.emit("chat", {
+        name:senderName.value,
+        message:message.value
+    })
+})
+
+socket.on("chat",function(data) {
+    messages.innerHTML+="<p><strong>"+data.name+": </strong>"+data.message+"</p>"
+})
