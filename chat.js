9a7735a1751a044eaefee863f59d2ac333d87cf9
Moorad <34781684+mooradal@users.noreply.github.com>
2018-10-11 22:15:00 +0100

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,24 +1,50 @@
-socket = io.connect("https://messaging-smimciwnjn.now.sh/");
+socket = io.connect("localhost:4000");
 
 var message = document.getElementById("message")
 var btn = document.getElementById("send")
 var senderName = document.getElementById("handle")
 var messages = document.getElementById("output")
+var key = document.getElementById("key")
+
+message.addEventListener("keypress",function(evnt) {
+    if (evnt.keyCode == 13) {
+        btn.click()
+    }
+})
 
 btn.addEventListener("click", function() {
+    if (senderName.value.toLowerCase().includes("moorad")) {
+        messages.innerHTML+="<p><strong class='error'>You Don't Have Admin Access to use the name Moorad</strong></p>"
+    }
+    else if (message.value != "" && senderName.value != "") {
     socket.emit("chat", {
         name:senderName.value,
         message:message.value
     })
-    message.value = "";
+    message.value = ""
+} else {
+    messages.innerHTML+='<p><strong class="error">Your Name/Message Input Field is empty</strong></p>'
+} 
 })
 
-message.onkeypress = function(e) {
-if (e.keyCode == 13) {
-        btn.click();
-    }
-}
-
 socket.on("chat",function(data) {
+    if (data.admin) {
+        messages.innerHTML+="<p><strong style='color:#ff751a'>"+data.name+": </strong>"+data.message+"</p>"
+    } else {
     messages.innerHTML+="<p><strong>"+data.name+": </strong>"+data.message+"</p>"
+    }
+    document.getElementById("chat-window").scrollTop = document.getElementById("chat-window").scrollHeight
+    
+})
+
+
+socket.on("online", function(connections) {
+    document.getElementById('online').innerText = "Online: "+connections
+})
+
+key.addEventListener("click",function() {
+    password = prompt("Admin Access Code: ")
+    if (password != null) {
+    socket.emit("password",password)
+    }   
 })
