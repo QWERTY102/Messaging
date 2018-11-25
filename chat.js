7be4c4dadf83ecb25ab8815db52b239fb0e82e06
Moorad <34781684+mooradal@users.noreply.github.com>
2018-11-25 21:46:01 +0000

    Messaging V2 Alpha is Out!! Gifs Are supported 🖼!

---
+++ chat.js
@@ -0,0 +1,118 @@
+var socket = io.connect('localhost:4000');
+var name;
+var imgBtn = document.querySelector('.imgBtn');
+var gifDiv = document.querySelector('.gifs');
+var sendBtn = document.querySelector('.send');
+var input = document.querySelector('.tinput');
+var ginput = document.querySelector('.ginput');
+var gBtn = document.querySelector('.gsend');
+var chatWindow = document.querySelector('.chat-window');
+var nameInput = document.querySelector('.name-input');
+var okButton = document.querySelector('.ok');
+var nameContainer = document.querySelector('.name-container');
+var server = 'https://messaging-tghbmeoaja.now.sh';
+
+okButton.addEventListener('click', () => {
+	name = nameInput.value;
+	nameContainer.style.display = 'none';
+});
+
+imgBtn.addEventListener('click', () => {
+	gifDiv.style.display = gifDiv.style.display == 'none' ? 'block' : 'none';
+	chatWindow.style.paddingBottom = gifDiv.style.display == 'none' ? '6em' : '26em';
+	chatWindow.scrollTo(0,chatWindow.scrollHeight);
+});
+
+input.addEventListener('keypress', (e) => {
+	if (e.keyCode == 13) {
+		socket.emit('messages', {
+			name:name,
+			message: input.value
+		});
+		input.value = '';
+	}
+});
+
+gBtn.addEventListener('click', () => {
+
+	fetch(`${server}/gsearch?term=${ginput.value}`, {
+		method: 'GET'
+	}).then(res => res.json())
+		.then(json => processImgsSearch(json.gifs));
+});
+
+sendBtn.addEventListener('click', () => {
+	socket.emit('messages', {
+		name:name,
+		message: input.value
+	});
+	input.value = '';
+});
+
+
+function processImgsSearch(data) {
+	var imgs = document.querySelectorAll('.send-img');
+	var iter = imgs.length;
+	for (var i = 0; i < iter; i++) {
+		document.getElementsByClassName('send-img')[0].parentElement.removeChild(document.getElementsByClassName('send-img')[0]);
+	}
+	console.log(data);
+	data.forEach((gif) => {
+		var img = document.createElement('img');
+		img.className = 'send-img';
+		img.src = server+'/gif?id='+gif;
+		img.onclick = function () {
+			send(this);
+		};
+		gifDiv.appendChild(img);
+	});
+}
+
+function processMessage(data) {
+	var message = document.createElement('span');
+	if (data.socket != socket.id) {
+		var messagerName = document.createElement('h3');
+		messagerName.innerHTML = `<strong>${data.name}</strong>`;
+		message.appendChild(messagerName);	
+	}
+	message.innerHTML += data.message;
+	message.className = data.socket == socket.id ? 'chat-message self' : 'chat-message';
+	chatWindow.appendChild(message);
+	chatWindow.scrollTo(0,chatWindow.scrollHeight);
+	console.log(socket.id);
+}
+
+function processImgs(data) {
+	var URL = data.img;
+	var container = document.createElement('span');
+	if (data.socket != socket.id) {
+		var messagerName = document.createElement('h3');
+		messagerName.className = 'imgname';
+		messagerName.innerHTML = `<strong>${data.name}</strong>`;
+		container.appendChild(messagerName);	
+	}
+	container.className = data.socket == socket.id ? 'chat-img selfimg' : 'chat-img';
+	var img = document.createElement('img');
+	img.src = URL;
+	container.appendChild(img);
+	chatWindow.appendChild(container);
+	chatWindow.scrollTo(0,chatWindow.scrollHeight);
+}
+
+function send(element) {
+	var URL = element.src;
+	console.log(URL);
+	socket.emit('img', {
+		name:name,
+		img: URL
+	});
+}
+
+socket.on('messages', (data) => {
+	processMessage(data);
+});
+
+
+socket.on('img', (data) => {
+	processImgs(data);
+});
